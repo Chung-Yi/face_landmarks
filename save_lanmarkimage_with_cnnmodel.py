f99a1391c1b4f550f48f1952c69ae08a595beb0f
Andy <wty81213@hotmail.com>
2019-07-06 01:20:26 +0800

    update

--- save_lanmarkimage_with_cnnmodel.py
+++ save_lanmarkimage_with_cnnmodel.py
@@ -13,8 +13,9 @@ parser.add_argument('--folder_path', help='choose a image folder')
 
 args = parser.parse_args()
 
+INPUT_SIZE = 200
 path = os.path.abspath(os.path.dirname(__file__))
-model_name = os.path.join(path, 'models/cnn_0701.h5')
+model_name = os.path.join(path, 'models/cnn_0628.h5')
 data_path = args.folder_path
 
 
@@ -42,12 +43,13 @@ def main():
 
         for image in glob.glob(os.path.join(data_path, '*.jpg')):
             img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+            image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
             if img is None:
                 continue
 
             try:
-                locations = fr.face_locations(img)
+                locations = fr.face_locations(image_rgb)
             except:
                 continue
 
@@ -60,29 +62,39 @@ def main():
                 locs.append((start_x, start_y, end_x, end_y))
 
             # crop from image
-            cut_face_imgs = cut_face(img, locs)
+            cut_face_imgs, delta_locs = cut_face(image_rgb, locs)
 
-            for face_img in cut_face_imgs:
+            for i, (face_img, delta_locs) in enumerate(
+                    zip(cut_face_imgs, delta_locs)):
                 image_name = image.split('/')[-1] + str(uuid4())[:3]
 
                 if face_img.size == 0:
                     continue
 
-                face_image = cv2.resize(face_img, (200, 200))
-                face_img = np.reshape(face_image, (1, 200, 200, 3))
-                face_img = face_img.astype('float32') / 255
+                face_resized = cv2.resize(face_img, (INPUT_SIZE, INPUT_SIZE))
+                face_reshape = np.reshape(face_resized,
+                                          (1, INPUT_SIZE, INPUT_SIZE, 3))
+                face_normalize = face_reshape.astype('float32') / 255
 
                 model = load_model(model_name)
-                points = model.predict(face_img)
-                points = np.reshape(points, (-1, 2)) * 200
+                points = model.predict(face_normalize)
+                points = np.reshape(points, (-1, 2))
+
+                if len(locs[i]) == 0:
+                    points = []
 
                 for point in points:
-                    cv2.circle(face_image, (int(point[0]), int(point[1])), 2,
+                    point[0] *= face_img.shape[1]
+                    point[1] *= face_img.shape[0]
+                    point[0] += locs[i][0] - delta_locs[0]
+                    point[1] += locs[i][1] - delta_locs[1]
+
+                for point in points:
+                    cv2.circle(img, (int(point[0]), int(point[1])), 2,
                                (0, 255, 0), -1, cv2.LINE_AA)
 
                     cv2.imwrite(
-                        './test/landmark_image/' + image.split('/')[-1],
-                        face_image)
+                        './test/landmark_image/' + image.split('/')[-1], img)
 
 
 if __name__ == "__main__":
