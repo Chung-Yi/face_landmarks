f9c28580c147b87521505dc3762bcfb7a20cf7d9
Andy <wty81213@hotmail.com>
2019-07-19 15:32:52 +0800

    update

--- detector/pose_detector.py
+++ detector/pose_detector.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import math
 from stabilizer import Stabilizer
+from utils import draw_landmak_point
 
 
 class PoseDetector:
@@ -229,3 +230,135 @@ class PoseDetector:
                  (255, 255, 255), 2, cv2.LINE_AA)
 
         return yaw
+
+    def nose_slop(self, points):
+        pt_top_x = points[27][0]
+        pt_top_y = points[27][1]
+
+        pt_bottom_x = points[30][0]
+        pt_bottom_y = points[30][1]
+
+        nose_m = (pt_top_y - pt_bottom_y) / (pt_top_x - pt_bottom_x)
+
+        cv2.putText(
+            self.face_image, ('nose_slop: {:02.2f}').format(
+                float(str(nose_m))), (10, 320),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 255, 0),
+            thickness=2,
+            lineType=2)
+        cv2.putText(
+            self.face_image, ('delta_x: {:02.2f}').format(
+                float(str(pt_top_x - pt_bottom_x))), (10, 360),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 255, 0),
+            thickness=2,
+            lineType=2)
+
+        return nose_m, abs(pt_top_x - pt_bottom_x)
+
+    def cheek_dist(self, points, nose_info):
+        left_cheek_x = points[2][0]
+        left_cheek_y = points[2][1]
+        right_cheek_x = points[14][0]
+        right_cheek_y = points[14][1]
+        nose_tip_x = points[30][0]
+        nose_tip_y = points[30][1]
+        left_cheek_x = points[2][0]
+        left_cheek_y = points[2][1]
+
+        left_dist = math.sqrt((left_cheek_x - nose_tip_x)**2 +
+                              (left_cheek_y - nose_tip_y)**2)
+        right_dist = math.sqrt((right_cheek_x - nose_tip_x)**2 +
+                               (right_cheek_y - nose_tip_y)**2)
+
+        if abs(left_dist - right_dist) < 30:
+            direction = 'm'
+        elif left_dist > right_dist:
+            direction = 'r'
+        elif right_dist > left_dist:
+            direction = 'l'
+
+        cv2.putText(
+            self.face_image, ('left_dist: {:05.2f}').format(
+                float(str(left_dist))), (10, 160),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 0, 255),
+            thickness=2,
+            lineType=2)
+
+        cv2.putText(
+            self.face_image, ('right_dist: {:05.2f}').format(
+                float(str(right_dist))), (10, 200),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 0, 0),
+            thickness=2,
+            lineType=2)
+
+        cv2.putText(
+            self.face_image, ('abs(left_dist - right_dist): {:05.2f}').format(
+                float(str(abs(left_dist - right_dist)))), (10, 240),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 255, 0),
+            thickness=2,
+            lineType=2)
+
+        cv2.putText(
+            self.face_image, ('nose_occ: {:05.2f}').format(
+                float(str(nose_info))), (10, 280),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 255, 0),
+            thickness=2,
+            lineType=2)
+
+        # print('right_dist', right_dist)
+        # print('left_dist', left_dist)
+        # print(abs(left_dist - right_dist))
+        return direction
+
+    def get_nose_angle(self, points):
+        p27 = points[27]
+        p30 = points[30]
+        p31 = points[31]
+        p35 = points[35]
+        p33 = points[33]
+
+        p30_p27 = p27 - p30
+        p33_p31 = p31 - p33
+        p33_p35 = p35 - p33
+
+        cosine_left_angle = np.dot(p30_p27, p33_p31)
+        cosine_right_angle = np.dot(p30_p27, p33_p35)
+
+    def triangle_area(self, image, points):
+
+        p27 = points[27]
+        p30 = points[30]
+        p31 = points[31]
+        p35 = points[35]
+
+        p30_p27 = p27 - p30
+        p30_p31 = p31 - p30
+        p30_p35 = p35 - p30
+
+        left_nose_area = 0.5 * abs(p30_p27[0] * p30_p31[1] -
+                                   p30_p27[1] * p30_p31[0])
+        right_nose_area = 0.5 * abs(p30_p27[0] * p30_p35[1] -
+                                    p30_p27[1] * p30_p35[0])
+
+        if abs(left_nose_area - right_nose_area) < 100:
+            direction = 'm'
+        elif left_nose_area > right_nose_area:
+            direction = 'r'
+        elif left_nose_area < right_nose_area:
+            direction = 'l'
+
+        cv2.putText(
+            self.face_image, ('delta_area: {:05.2f}').format(
+                float(str(abs(left_nose_area - right_nose_area)))), (10, 400),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1, (0, 255, 0),
+            thickness=2,
+            lineType=2)
+
+        return direction
